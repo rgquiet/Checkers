4674dd853aa3fcfa4021c9e1c3556bb8f013d88e
Poi <paul.guedel@students.ffhs.ch>
2019-05-13 00:03:14 +0200

    King: - Started on Jump Logic

--- src/King.java
+++ src/King.java
@@ -14,7 +14,37 @@ public class King extends Piece {
 
     @Override
     boolean jump(ArrayList<Integer> start, Image king) {
-        //wip...
+        ArrayList<ArrayList> posMoves = new ArrayList<ArrayList>();
+        // start = int of field on Playground
+        ArrayList<Integer> steps = new ArrayList<Integer>();
+        steps.add(start.get(0));
+
+        //Check Northeast
+        checkNorthEast(steps, steps.get(steps.size() - 1));
+        //Check Southeast
+
+        //Check Southwest
+
+        //Check Northwest
+
+
+        return false;
+    }
+
+    boolean checkNorthEast(ArrayList steps, Integer start){
+        for(int i = 1; i < 10; i++){
+            if(!getPlayer().getGame().getPlayground().get(start + i * 11).getChildren().isEmpty() && getPlayer().getGame().getPlayground().get(start + (i + 1) * 11).getChildren().isEmpty()){
+                Piece target = (Piece)getPlayer().getGame().getPlayground().get(start + i * 11).getChildren();
+                if(target.getPlayer() != getPlayer()){
+                    steps.add(getPlayer().getGame().getPlayground().indexOf(getPlayer().getGame().getPlayground().get(start + (i + 1) * 11)));
+                    checkNorthEast(steps,(int)steps.get(steps.size() - 1));
+                    return true;
+                }
+                else{
+                    break;
+                }
+            }
+        }
         return false;
     }
 
