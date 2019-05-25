03a4a9666739e5fe5f596e6ebdc278adc692300b
Poi <paul.guedel@students.ffhs.ch>
2019-05-25 11:42:10 +0200

    Qlearn: - Created Class

---
+++ src/Qlearn.java
@@ -0,0 +1,18 @@
+import javafx.scene.layout.Pane;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Qlearn implements Runnable{
+
+    ArrayList<Pane> playground = new ArrayList<>();
+    HashMap<String, ArrayList<Integer>> Q = new HashMap<>();
+
+
+    @Override
+    public void run() {
+
+
+
+    }
+}
